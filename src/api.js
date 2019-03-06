a0b88e4d4a907956f8298699b87adc9ea440d865
ZhangYuS <834544739@qq.com>
2019-03-07 00:29:10 +0800

    zzw完成post请求，可以使用

--- src/api.js
+++ src/api.js
@@ -1,11 +1,20 @@
 export async function query (v) {
   const params = new URLSearchParams()
   params.append('p', v)
+  console.log('res')
   const res = await window.axios({
     method: 'post',
-    url: 'http://127.0.0.1:5000/search/',
+    url: './search/',
     data: params
+  }).then(
+    function (response) {
+      console.log('response')
+      if (response.status === 200) {
+        return response.data
+      }
+    }
+  )['catch']((error) => {
+    console.log(error)
   })
-  console.log(res)
   return res
 }
