b0db49657db089ed92f903fff02548d6b5b558eb
SYRain <834544739@qq.com>
2019-03-14 23:15:48 +0800

    删除后台

--- backend/KG/test.py
+++
@@ -1,9 +0,0 @@
-from knowGraf1_2.Interpreter import Interpreter
-DBNAME = "disease"
-
-
-key_str = "肺炎"
-complier = Interpreter(DBNAME, key_str)
-fun, args = complier.query_to_triples(key_str)
-res, re_str = complier.run()
-print(res, re_str)
